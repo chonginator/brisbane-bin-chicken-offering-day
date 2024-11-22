50040d412377666ecbc970ff2ac54cc72bfcadd3
chonginator <benchong1999@yahoo.com.au>
2024-11-22 15:35:22 +1000

    Add basic page template and html helpers

---
+++ html.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func respondWithError(w http.ResponseWriter, code int, msg string, err error) {
+	if err != nil {
+		log.Println(err)
+	}
+
+	if code > 499 {
+		log.Printf("Responding with 5XX error: %s", msg)
+	}
+
+	tmpl, err := template.ParseFiles("error.html")
+	if err != nil {
+		http.Error(w, "Coudln't load error template", http.StatusInternalServerError)
+		return
+	}
+	respondWithHTML(w, code, tmpl, msg)
+}
+
+func respondWithHTML(w http.ResponseWriter, code int, tmpl *template.Template, data any) {
+	w.Header().Set("Content-Type", "text/html")
+
+	err := tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, "Coudln't execute template", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(code)
+}
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 	"time"
@@ -8,10 +9,10 @@ import (
 
 func main() {
 	const port = "8080"
-	const rootFilePath = "."
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(rootFilePath)))
+	mux.HandleFunc("/", handlerRoot)
+	mux.HandleFunc("/suburbs", handlerSuburbs)
 
 	srv := &http.Server{
 		Addr:              ":" + port,
@@ -22,4 +23,24 @@ func main() {
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func handlerRoot(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/suburbs", http.StatusMovedPermanently)
+}
+
+func handlerSuburbs(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	type PageData struct {
+		Title string
+	}
+
+	tmpl, err := template.ParseFiles("layout.html", "index.html")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't load template", err)
+		return
+	}
+
+	respondWithHTML(w, http.StatusOK, tmpl, PageData{Title: "Suburbs"})
 }
