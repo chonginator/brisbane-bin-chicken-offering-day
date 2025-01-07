684473db41af6616ab041bc73f006927d7ac6e6a
chonginator <benchong1999@yahoo.com.au>
2025-01-07 20:32:11 +1000

    Move template parsing from handler to API initialization

--- internal/api/config.go
+++ internal/api/config.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"html/template"
+	"path/filepath"
 
 	"github.com/chonginator/brisbane-bin-chicken-offering-day/internal/database"
 )
@@ -11,12 +13,13 @@ import (
 type Config struct {
 	db          *database.Queries
 	suburbNames []string
+	templates   map[string]*template.Template
 }
 
 func NewAPIConfig(dbURL string) (*Config, error) {
 	db, err := sql.Open("libsql", dbURL)
 	if err != nil {
-		return nil, fmt.Errorf("error connecting to database: %v", err)
+		return nil, fmt.Errorf("error connecting to database: %w", err)
 	}
 	defer db.Close()
 
@@ -24,7 +27,7 @@ func NewAPIConfig(dbURL string) (*Config, error) {
 
 	suburbs, err := dbQueries.GetSuburbs(context.Background())
 	if err != nil {
-		return nil, fmt.Errorf("error getting suburbs from database: %v", err)
+		return nil, fmt.Errorf("error getting suburbs from database: %w", err)
 	}
 
 	suburbNames := make([]string, 0, len(suburbs))
@@ -32,10 +35,43 @@ func NewAPIConfig(dbURL string) (*Config, error) {
 		suburbNames = append(suburbNames, suburb.Name)
 	}
 
+	templates, err := parseTemplates()
+	if err != nil {
+		return nil, fmt.Errorf("error parsing templates: %w", err)
+	}
+
 	apiCfg := &Config{
 		db:          dbQueries,
 		suburbNames: suburbNames,
+		templates:   templates,
 	}
 
 	return apiCfg, nil
+}
+
+func parseTemplates() (map[string]*template.Template, error) {
+	files, err := filepath.Glob("templates/*.html")
+	if err != nil {
+		return nil, fmt.Errorf("error finding templates: %w", err)
+	}
+
+	templates := make(map[string]*template.Template)
+	layoutFile := "templates/layout.html"
+
+	for _, file := range files {
+		if file == layoutFile {
+			continue
+		}
+
+		name := filepath.Base(file)
+		tmpl, err := template.ParseFiles(layoutFile, file)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing template: %w", err)
+		}
+
+		templates[name] = tmpl
+	}
+
+	fmt.Println(templates)
+	return templates, nil
 }
--- internal/api/handler_suburbs.go
+++ internal/api/handler_suburbs.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-	"html/template"
 	"net/http"
 )
 
@@ -12,16 +11,9 @@ type SuburbsPageData struct {
 func (cfg *Config) HandlerSuburbs(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 
-	// TODO: Move template parsing outside of handler?
-	tmpl, err := template.ParseFiles("templates/layout.html", "templates/index.html")
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Couldn't load template", err)
-		return
-	}
-
 	data := SuburbsPageData{
 		SuburbNames: cfg.suburbNames,
 	}
 
-	respondWithHTML(w, http.StatusOK, tmpl, data)
+	respondWithHTML(w, http.StatusOK, cfg.templates["index.html"], data)
 }
