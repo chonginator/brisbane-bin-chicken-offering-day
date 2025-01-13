a744d04adaea69d32fb5fc322cca7c09163cf1ad
chonginator <benchong1999@yahoo.com.au>
2025-01-13 21:08:51 +1000

    Add streets table migration

---
+++ sql/schema/003_streets.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+CREATE TABLE streets (
+  id UUID PRIMARY KEY,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
+  street_name TEXT NOT NULL,
+  suburb_id UUID NOT NULL,
+  FOREIGN KEY (suburb_id) REFERENCES suburbs(id) ON DELETE CASCADE
+);
+
+-- +goose Down
+DROP TABLE streets;
