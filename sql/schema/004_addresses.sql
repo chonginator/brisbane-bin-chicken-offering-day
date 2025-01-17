33eca5d006396e2ddc8c222664eb06cd1f7f6007
chonginator <benchong1999@yahoo.com.au>
2025-01-17 22:04:47 +1000

    Add addresses table schema

---
+++ sql/schema/004_addresses.sql
@@ -0,0 +1,19 @@
+-- +goose Up
+CREATE TABLE addresses (
+  id UUID PRIMARY KEY,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
+  property_id TEXT NOT NULL UNIQUE,
+  unit_number TEXT,
+  house_number TEXT NOT NULL,
+  house_number_suffix TEXT,
+  street_id UUID NOT NULL,
+  collection_day TEXT NOT NULL,
+  zone TEXT NOT NULL,
+  FOREIGN KEY (street_id) REFERENCES streets(id) ON DELETE CASCADE,
+  CHECK (collection_day IN ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday')),
+  CHECK (zone IN ('1', '2'))
+);
+
+-- +goose Down
+DROP TABLE addresses;
