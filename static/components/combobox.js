878afd64cb01346efddd7eec305655b63b490a43
chonginator <benchong1999@yahoo.com.au>
2025-05-26 21:26:53 +1000

    Extract combobox component and add boilerplate javascript

---
+++ static/components/combobox.js
@@ -0,0 +1,34 @@
+function combobox(tree = document) {
+  tree.querySelectorAll("[data-combobox]").forEach(comboboxRoot => {
+    const combobox = comboboxRoot.querySelector("[role=combobox]")
+    const listbox = comboboxRoot.querySelector("[role=listbox]")
+    const options = [...listbox.querySelectorAll("[role=option]")]
+
+    const isOpen = () => !listbox.hidden
+
+    function toggleCombobox(open = !isOpen()) {
+      console.log(`toggle combobox to: ${open}`)
+      if (open) {
+        listbox.hidden = false
+        combobox.setAttribute("aria-expanded", true)
+      } else {
+        listbox.hidden = true
+        combobox.setAttribute("aria-expanded", false)
+      }
+    }
+
+    comboboxRoot.addEventListener("focus", e => {
+      toggleCombobox(true)
+    }, { capture: true })
+
+    comboboxRoot.addEventListener("blur", e => {
+      if (!comboboxRoot.contains(document.activeElement)) {
+        toggleCombobox(false)
+      }
+    }, { capture: true })
+    
+    toggleCombobox(isOpen())
+  })
+}
+
+addEventListener("htmx:load", e => combobox(e.target))
