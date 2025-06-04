8ca2d604abf26eb479188903dd7dca3a61df3793
chonginator <benchong1999@yahoo.com.au>
2025-06-04 21:30:35 +1000

    Add combobox popup keyboard up and down interaction

--- static/components/combobox.js
+++ static/components/combobox.js
@@ -4,43 +4,75 @@ function combobox(tree = document) {
     const listbox = comboboxRoot.querySelector("[role=listbox]")
     const options = [...listbox.querySelectorAll("[role=option]")]
 
+    const controller = new AbortController()
+    const signal = controller.signal
+
+    listbox.addEventListener("htmx:beforeSwap", () => {
+      controller.abort()
+    })
+
     const isOpen = () => !listbox.hidden
+    
+    const getSelectedOptionIndex = () => options.findIndex(option => option.getAttribute("aria-selected") === "true")
 
     comboboxRoot.addEventListener("focus", e => {
       toggleCombobox(true)
-    }, { capture: true })
+    }, { capture: true, signal: signal })
 
     comboboxRoot.addEventListener("blur", e => {
       if (!comboboxRoot.contains(document.activeElement)) {
         toggleCombobox(false)
       }
-    }, { capture: true })
-    
+    }, { capture: true, signal: signal })
+
+    comboboxRoot.addEventListener("keydown", e => {
+      switch (e.key) {
+        case "ArrowDown": {
+          const nextOptionIndex = getSelectedOptionIndex() === options.length - 1 ? 0 : getSelectedOptionIndex() + 1
+          const nextOption = options.at(nextOptionIndex)
+          selectOption(nextOption)
+          nextOption.scrollIntoView({ block: "nearest", inline: "nearest" })
+          break
+        }
+        case "ArrowUp": {
+          const prevOption = options.at(getSelectedOptionIndex() - 1)
+          selectOption(prevOption)
+          prevOption.scrollIntoView({ block: "nearest", inline: "nearest" })
+          break
+        }
+      }
+    }, { signal: signal })
+
+    listbox.addEventListener("mouseover", e => {
+      const option = e.target.closest("[role=option]")
+      if (option) {
+        selectOption(option)
+      }
+    }, { signal: signal })
+
     toggleCombobox(isOpen())
 
     function toggleCombobox(open = !isOpen()) {
       if (open) {
         listbox.hidden = false
         combobox.setAttribute("aria-expanded", true)
+        selectOption(options[0])
       } else {
         listbox.hidden = true
         combobox.setAttribute("aria-expanded", false)
       }
     }
 
-    options.forEach(option => {
-      option.addEventListener("mouseover", () => {
-        selectOption(option)
-      })
-    })
-
     function selectOption(option = options[0]) {
       combobox.setAttribute("aria-activedescendant", option.id)
+
       const unselectedOptions = options.filter(o => !Object.is(o, option))
       unselectedOptions.forEach(o => o.setAttribute("aria-selected", false))
+
       option.setAttribute("aria-selected", true)
     }
   })
 }
 
-addEventListener("htmx:load", e => combobox(e.target))
+addEventListener("DOMContentLoaded", () => combobox())
+addEventListener("htmx:afterSwap", () => combobox())
