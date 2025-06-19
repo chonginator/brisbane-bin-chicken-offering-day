b60bb2dd9f7a25ee39e6f8dd41640ca4e3a16969
chonginator <benchong1999@yahoo.com.au>
2025-06-19 21:54:00 +1000

    Add FTS5 virtual table for searching addresses

--- sql/queries/addresses.sql
+++ sql/queries/addresses.sql
@@ -48,42 +48,47 @@ LIMIT :batch_size
 OFFSET :offset;
 
 -- name: SearchAddresses :many
-WITH address_data AS (
-  SELECT 
-    a.property_id,
-    a.collection_day,
-    a.zone,
-    CAST(
-      CASE 
-        WHEN a.unit_number IS NOT NULL AND a.unit_number != '' 
-        THEN a.unit_number || '/' || a.house_number 
-        ELSE a.house_number 
-      END || 
-      CASE 
-        WHEN a.house_number_suffix IS NOT NULL AND a.house_number_suffix != '' 
-        THEN a.house_number_suffix 
-        ELSE '' 
-      END || ' ' || s.name || ', ' || sub.name
-    AS VARCHAR) AS formatted_address
-  FROM addresses a
-  JOIN streets s ON a.street_id = s.id
-  JOIN suburbs sub ON s.suburb_id = sub.id
-  WHERE 
-    (
-      COALESCE(a.unit_number, '') || 
-      '/' ||
-      COALESCE(a.house_number, '') || 
-      COALESCE(a.house_number_suffix, '') || 
-      ' ' ||
-      s.name || 
-      ', ' ||
-      sub.name
-    ) LIKE LOWER('%' || :query || '%')
-)
-SELECT 
-  property_id,
-  collection_day,
-  zone,
-  formatted_address
-FROM address_data
+SELECT property_id, search_text AS formatted_address
+FROM address_search
+WHERE search_text MATCH :query
 LIMIT :limit;
+
+-- WITH address_data AS (
+--   SELECT 
+--     a.property_id,
+--     a.collection_day,
+--     a.zone,
+--     CAST(
+--       CASE 
+--         WHEN a.unit_number IS NOT NULL AND a.unit_number != '' 
+--         THEN a.unit_number || '/' || a.house_number 
+--         ELSE a.house_number 
+--       END || 
+--       CASE 
+--         WHEN a.house_number_suffix IS NOT NULL AND a.house_number_suffix != '' 
+--         THEN a.house_number_suffix 
+--         ELSE '' 
+--       END || ' ' || s.name || ', ' || sub.name
+--     AS VARCHAR) AS formatted_address
+--   FROM addresses a
+--   JOIN streets s ON a.street_id = s.id
+--   JOIN suburbs sub ON s.suburb_id = sub.id
+--   WHERE 
+--     (
+--       COALESCE(a.unit_number, '') || 
+--       '/' ||
+--       COALESCE(a.house_number, '') || 
+--       COALESCE(a.house_number_suffix, '') || 
+--       ' ' ||
+--       s.name || 
+--       ', ' ||
+--       sub.name
+--     ) LIKE LOWER('%' || :query || '%')
+-- )
+-- SELECT 
+--   property_id,
+--   collection_day,
+--   zone,
+--   formatted_address
+-- FROM address_data
+-- LIMIT :limit;
